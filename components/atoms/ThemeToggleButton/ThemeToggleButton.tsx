20151f6e703366454cbc4df5c42480a5d976eb81
Hassan Awada <hassanawda301@gmail.com>
2025-07-18 15:47:20 +0300

    Update the bestPrice and the ToggleThemeButton

--- components/atoms/ThemeToggleButton/ThemeToggleButton.tsx
+++ components/atoms/ThemeToggleButton/ThemeToggleButton.tsx
@@ -13,7 +13,7 @@ const ThemeToggleButton = () => {
       onClick={() => dispatch(toggleTheme())}
       className="p-2 px-4 rounded bg-gray-200 dark:bg-gray-700 text-black dark:text-white"
     >
-      {theme === "dark" ? "☀️ Light Mode" : "🌙 Dark Mode"}
+      {theme === "dark" ? "☀️" : "🌙"}
     </button>
   );
 };
